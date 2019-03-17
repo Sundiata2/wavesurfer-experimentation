facb16d34d2994144d51ed8332571ab3ced2942f
Kevin <kevinlbelanger@gmail.com>
2019-03-17 17:30:04 -0400

    get current time and duration and display

--- src/components/wavesurfer-component.tsx
+++ src/components/wavesurfer-component.tsx
@@ -9,6 +9,8 @@ class WaveSurf extends React.Component<{
   isPlaying?: boolean,
   wavesurfer?: any,
   ready?: boolean,
+  currentTime?: string,
+  duration?: string
 }> {
 
   constructor(props:any) {
@@ -17,7 +19,9 @@ class WaveSurf extends React.Component<{
     this.state = {
       isPlaying: false,
       wavesurfer: null,
-      ready: false
+      ready: false,
+      currentTime: '0',
+      duration: '0'
     };
   }
 
@@ -45,9 +49,16 @@ class WaveSurf extends React.Component<{
     wavesurfer.load(`${process.env.PUBLIC_URL}/audio/waitin.mp3`);
 
     wavesurfer.on('ready', () => {
-      this.setState({ready: true}), () => {
+      const duration = wavesurfer.getDuration();
+      this.setState({ready: true, duration}), () => {
         wavesurfer.play();
       }
+    });
+
+    wavesurfer.on('audioprocess', () => {
+      this.setState({
+        currentTime: wavesurfer.getCurrentTime()
+      });
     });
 
     this.setState({
@@ -72,7 +83,7 @@ class WaveSurf extends React.Component<{
   }
 
   public renderWaveSurfer() {
-    const {ready} = this.state;
+    const {ready, wavesurfer} = this.state;
     if (!ready) {
       return (
         <div id="waveform-container" style={{width: '500px'}}>
@@ -93,6 +104,8 @@ class WaveSurf extends React.Component<{
         {this.renderWaveSurfer()}
         <div onClick={this.play.bind(this)} style={{cursor: 'pointer'}}>{'Play'}</div>
         <div onClick={this.pause.bind(this)} style={{cursor: 'pointer'}}>{'Pause'}</div>
+        <div>{this.state.currentTime}</div>
+        <div>{this.state.duration}</div>
       </div>
     );
   }
